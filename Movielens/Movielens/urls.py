dc2a0df0a51c29724abc90aea1a23397498784a4
Dy <Dy@Dylan>
2016-12-15 13:00:21 +0100

    Ejercicio Django acabado

--- Movielens/Movielens/urls.py
+++ Movielens/Movielens/urls.py
@@ -7,8 +7,8 @@ urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$','principal.views.inicio'),
-    url(r'^peliculas/$','principal.views.peliculas'),)
-#     url(r'^usuarios/$','principal.views.usuarios'),
-#     url(r'^buscarPelicula/$','principal.views.peliculaAno'),
+    url(r'^peliculas/$','principal.views.peliculas'),
+    url(r'^usuarios/$','principal.views.usuarios'),
+    url(r'^categorias/$','principal.views.categorias'),)
 #     url(r'^buscarPuntuacionPorUsuario/$','principal.views.idUsuario'),
 #     url(r'^usuarioMasPeliculasHaPuntuado/$','principal.views.usuarioMasPeliculasHaPuntuado'),)
--- Movielens/principal/views.py
+++ Movielens/principal/views.py
@@ -11,3 +11,19 @@ def peliculas(request):
    
     return render_to_response('peliculas.html', {'peliculas':peliculas})
 
+def categorias(request):
+    categorias = Categoria.objects.all()
+   
+    return render_to_response('categorias.html', {'categorias':categorias})
+
+def usuarios(request):
+    usuarios = []
+    conn = sqlite3.connect('movielens.db')
+    cursor = conn.execute("""SELECT * FROM principal_usuario group by codigoPostal""")
+    
+    for i in cursor:
+        usuario = Usuario.objects.get(pk=i[0])
+        usuarios.append(usuario)
+
+   
+    return render_to_response('usuarios.html', {'usuarios':usuarios})
