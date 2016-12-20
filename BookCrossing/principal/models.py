ee0395e230aaafb009d8dee894d7db6ce438db83
Dy <Dy@Dylan>
2016-12-20 12:31:18 +0100

    MOdelo y base de datos creadas

--- BookCrossing/principal/models.py
+++ BookCrossing/principal/models.py
@@ -1,14 +1,10 @@
 #encoding:utf-8
 
 from django.db import models
-    
-class Ocupacion(models.Model):
-    nombre = models.CharField(max_length=50)
-    def _unicode_(self):
-        return self.nombre
+from pattern.db import primary_key
     
 class Usuario(models.Model):
-    idUsuario = models.CharField(max_length=50)
+    idUsuario = models.CharField(primary_key=True,max_length=50)
     edad = models.TextField()
     localizacion = models.TextField(max_length=50)
    
@@ -17,28 +13,23 @@ class Usuario(models.Model):
     
 
 
-class Categoria(models.Model):
-    idCategoria = models.IntegerField()
-    nombre =  models.CharField(max_length=50)
-
-    def _unicode_(self):
-        return self.nombre
-      
 
     
-class Pelicula(models.Model):
-    idPelicula = models.CharField(max_length=50)
+class Libro(models.Model):
+    ISBN = models.CharField(primary_key=True,max_length=50)
     titulo = models.TextField()
-    fechaEstreno = models.CharField(max_length=50)
-    IMDbURL= models.URLField()
-    categorias=models.ManyToManyField(Categoria)
-    puntuacion=models.ManyToManyField(Usuario, through='Puntuacion')
+    autor = models.TextField()
+    anoPublicacion = models.CharField(max_length=50)
+    editor = models.TextField()
+    url_s= models.URLField()
+    url_m= models.URLField()
+    url_l= models.URLField()
     
     def _unicode_(self):
-        return self.idPelicula
+        return self.isbn
     
     
 class Puntuacion(models.Model):
-    usuario = models.ForeignKey(Usuario)
-    pelicula = models.ForeignKey(Pelicula,related_name='pelicula')
-    cantidad = models.IntegerField()
+    idUsuario = models.ForeignKey(Usuario)
+    isbn = models.ForeignKey(Libro)
+    cantidad = models.IntegerField(range(1,10))
