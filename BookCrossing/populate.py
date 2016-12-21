b0162d915f8a893190868760d8278b611b0b9296
Juan Jesus Vazquez Rodriguez <Juan Jesus Vazquez Rodriguez>
2016-12-21 16:33:06 +0100

    Arreglado pryecto

--- BookCrossing/populate.py
+++ BookCrossing/populate.py
@@ -1,94 +1,52 @@
+#encoding:utf-8
 import sqlite3
-from datetime import datetime
+import re
 
-def insertaOcupacion():
-    conn = sqlite3.connect('movielens.db')
-    conn.execute("""DELETE FROM principal_ocupacion""")
-    conn.text_factory = str
-    ocupaciones = open('u.occupation', 'r')
-    for u in ocupaciones.readlines():
-        conn.execute("""INSERT INTO principal_ocupacion (nombre) VALUES (?)""",(u,))
-    conn.commit()
-    conn.close()
-    
-def insertaCategoria():
-    conn = sqlite3.connect('movielens.db')
-    conn.execute("""DELETE FROM principal_categoria""")
-    conn.text_factory = str
-    categorias = open('u.genre', 'r')
-    for c in categorias.readlines():
-        aux = c.split('|')
-        if len(aux)==2:
-            conn.execute("""INSERT INTO principal_categoria (idCategoria,nombre) VALUES (?,?)""",(aux[1],aux[0],))
-        else:
-            pass
-    conn.commit()
-    conn.close()
-
-def insertaPelicula():
-    ids_categorias = []
-    conn = sqlite3.connect('movielens.db')
-    conn.execute("""DELETE FROM principal_pelicula""")
-    conn.execute("""DELETE FROM principal_pelicula_categoria""")
-    conn.text_factory = str
-    peliculas = open('u.item', 'r')
-    for p in peliculas.readlines():
-        aux = p.split('|')
-        for i in range(5,24):
-            if aux[i]==str(1):
-                ids_categorias.append((i-4))
-        conn.execute("""INSERT INTO principal_pelicula (idPelicula,titulo, fechaEstreno, imdbURL) VALUES (?,?,?,?)""",
-                     (aux[0],aux[1],convierteFecha(aux[2]),aux[4]))
-        for id in ids_categorias:
-            conn.execute("""INSERT INTO principal_pelicula_categoria (pelicula_id, categoria_id) VALUES (?,?)""",
-                         (aux[0], id))
-        ids_categorias=[]
-
-
-    conn.commit()
-    conn.close()
-    
 def insertaUsuario():
-    conn = sqlite3.connect('movielens.db')
+    conn = sqlite3.connect('bookCrossing.db')
     conn.execute("""DELETE FROM principal_usuario""")
     conn.text_factory = str
-    usuarios = open('u.user', 'r')
+    usuarios = open('BX-Users.csv','r')
     for u in usuarios.readlines():
-        tmp = u.split('|')
-        cursor = conn.execute("""select * from principal_ocupacion where nombre like ?""",(tmp[3]+'\n',))
-        for i in cursor:
-            ocupacion_id =i[0]
-
-        conn.execute("""INSERT INTO principal_usuario (idUsuario, edad, sexo, ocupacion_id, codigoPostal) VALUES (?,?,?,?,?)""",
-                     (tmp[0],tmp[1],tmp[2],ocupacion_id,tmp[4]))
+        tmp = u.split(';')
+        conn.execute("""INSERT INTO principal_usuario(idUsuario, localizacion, edad) VALUES (?,?,?)""",(cleanString(tmp[0]),cleanString(tmp[1].decode('cp1252').encode('utf-8')),cleanString(tmp[2].decode('cp1252').encode('utf-8'))))
     conn.commit()
     conn.close()
-    
+    print("Usuarios insertados")
+
+def insertaLibro():
+    conn = sqlite3.connect('bookCrossing.db')
+    conn.execute("""DELETE FROM principal_libro""")
+    conn.text_factory = str
+    libros = open('BX-Books.csv','r')
+    for l in libros.readlines():
+        tmp = l.split(';')
+        conn.execute("""INSERT INTO principal_libro(ISBN,titulo,autor,publicacion,editor,urlS,urlM,urlL) VALUES(?,?,?,?,?,?,?,?)""",
+                     (cleanString(tmp[0]),cleanString(tmp[1].decode('cp1252').encode('utf-8')),cleanString(tmp[2].decode('cp1252').encode('utf-8')),cleanString(tmp[3]),cleanString(tmp[4].decode('cp1252').encode('utf-8')),cleanString(tmp[5]),cleanString(tmp[6]),cleanString(tmp[7])))
+    conn.commit()
+    conn.close()
+    print("Libros insertados")
+
+
 def insertaPuntuacion():
-    conn = sqlite3.connect('movielens.db')
+    conn = sqlite3.connect('bookCrossing.db')
     conn.execute("""DELETE FROM principal_puntuacion""")
     conn.text_factory = str
-    puntuaciones = open('u.data', 'r')
+    puntuaciones = open('BX-Book-Ratings.csv', 'r')
     for p in puntuaciones.readlines():
-        tmp = p.split(' ')[0].split('\t')
-        conn.execute("""INSERT INTO principal_puntuacion (usuario_id, pelicula_id, cantidad) VALUES (?,?,?)""",
-                     (tmp[0],tmp[1],tmp[2]))
+        tmp = p.split(';')
+        conn.execute("""INSERT INTO principal_puntuacion (usuario_id,libro_id,puntuacion) VALUES (?,?,?)""",(cleanString(tmp[0]),cleanString(tmp[1]),cleanString(tmp[2])))
     conn.commit()
     conn.close()
-        
-def convierteFecha(fecha):
-    aux = fecha.split('-')
-    date_object = ''
-    if len(aux)==3:
-        fecha = aux[1]+' '+aux[0]+' '+aux[2]
-        date_object = datetime.strptime(fecha, '%b %d %Y')
-    return date_object
-    
-def main():
-    insertaOcupacion()
-    insertaCategoria()
-    insertaPelicula()
+    print("Puntuaciones insertadas")
+
+
+def cleanString(texto):
+    result =re.sub(r"[\"]", "", texto)
+    return result
+
+
+if __name__ == '__main__':
     insertaUsuario()
+    insertaLibro()
     insertaPuntuacion()
-if __name__ == "__main__":
-    main()
--- BookCrossing/principal/views.py
+++ BookCrossing/principal/views.py
@@ -1,29 +1,40 @@
-from principal.models import Pelicula,Usuario,Categoria
+#enconding:utf-8
+from principal.models import Libro,Puntuacion,Usuario
 from django.shortcuts import render_to_response
+from principal.forms import LibrosPuntutadosUsuarioForm
+from django.template import RequestContext
 import sqlite3
 
 
 def inicio(request):
     return render_to_response('inicio.html')
 
-def peliculas(request):
-    peliculas = Pelicula.objects.all()
-   
-    return render_to_response('peliculas.html', {'peliculas':peliculas})
+# def peliculas(request):
+#     peliculas = Libro.objects.all()
+#    
+#     return render_to_response('peliculas.html', {'peliculas':peliculas})
 
 def categorias(request):
-    categorias = Categoria.objects.all()
+    categorias = Libro.objects.all()
    
     return render_to_response('categorias.html', {'categorias':categorias})
 
-def usuarios(request):
-    cPs = []
-    usuarios = Usuario.objects.all()
-   
-    for u in usuarios.all():
-        if u.codigoPostal not in cPs:
-            cPs.append(u.codigoPostal)
-
-   
-    return render_to_response('usuarios.html', {'usuarios':usuarios,'cps':cPs})
+def buscaPuntuacionesUsuarios(request):
+    resultado = []
+    conn = sqlite3.connect('bookCrossing.db')
+    
+    if request.method == 'POST':
+        formulario = LibrosPuntutadosUsuarioForm(request.POST)
+        if formulario.is_valid():
+            formulario = LibrosPuntutadosUsuarioForm(request.POST)
+            if formulario.is_valid():
+                id = formulario.cleaned_data['idUsuario']
+                puntuaciones = Puntuacion.objects.filter(usuario=id)#aqui falla
+                for p in puntuaciones:
+                    peli = conn.execute("""SELECT (titulo, ISBN) from principal_libro where ISBN = ?;""", (p.libro))
+                    resultado.append(peli[0]+" - "+peli[1]+":  "+str(p.cantidad))
+                return render_to_response('librosPorPuntuacion.html', {'resultado': resultado})
+    else:
+        formulario = LibrosPuntutadosUsuarioForm()
+        return render_to_response('formPuntuacionesUsuario.html', {'formulario': formulario}, context_instance=RequestContext(request))
 
